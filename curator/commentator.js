699f4242e9ab535c254dbb459e2181227e4d399f
iyaki <ivanyaki96@gmail.com>
2023-12-10 15:50:06 -0300

    Actualizacion openai sdk en curator

--- curator/commentator.js
+++ curator/commentator.js
@@ -1,17 +1,17 @@
-import { Configuration, OpenAIApi } from 'openai'
+import OpenAIApi from 'openai'
 
-const openai = new OpenAIApi(new Configuration({
+const openai = new OpenAIApi({
 	apiKey: process.env.OPENAI_API_KEY,
-}));
+});
 
 export async function getComment(excerpt) {
-	const completion = await openai.createChatCompletion({
-		model: "gpt-3.5-turbo",
+	const completion = await openai.chat.completions.create({
+		model: 'gpt-3.5-turbo',
 		messages: [{
 			role: 'user',
-			content: `Write, in spanish, in a non personal way (definitively not in first person), a short (no more than two short sentences) catchy epigraph for this article: ${excerpt.substring(0, 17000)}`,
+			content: `Write, in spanish, in a non personal way (definitively not in first person), with a simple language, a short (no more than two or three short sentences), catchy epigraph for this article: ${excerpt.substring(0, 17000)}`,
 		}]
 	})
 
-	return completion.data.choices[0].message.content
+	return completion.choices[0].message.content
 }
